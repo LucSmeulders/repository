a21e56357f26e3ff0e8219e52f02aae988e3f7b8
Luc <luc@axito.com>
2019-02-01 19:33:58 +0100

    t

--- Syntra/Backend-Developer-1/05-PHP/Module-2/jquery-opdrachten-1/Opdracht_1/main.js
+++ Syntra/Backend-Developer-1/05-PHP/Module-2/jquery-opdrachten-1/Opdracht_1/main.js
@@ -1,4 +1,12 @@
  // Write your solution here
+<<<<<<< HEAD
  $(document).ready(function() {
    $('#text').hide();
  });
+=======
+
+
+ $(document).ready(function(){
+    $("#text").hide();
+ });
+>>>>>>> 4a66a6cec13641e29330c69ee37594536f27a7dd
